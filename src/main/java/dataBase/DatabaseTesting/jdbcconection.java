6d8327db727ee1c12c9ef768442bb791c88aa653
ricriospino <ricriospino@gmail.com>
2023-05-07 01:40:39 -0500

    commit rama origi

--- src/main/java/dataBase/DatabaseTesting/jdbcconection.java
+++ src/main/java/dataBase/DatabaseTesting/jdbcconection.java
@@ -31,6 +31,7 @@ public class jdbcconection {
 
 		driver.findElement(By.xpath(".//*[@id='username']")).sendKeys(rs.getString("username"));
 		driver.findElement(By.xpath(".//*[@id='password']")).sendKeys(rs.getString("password"));
+		System.out.println("Good");
 
 		}		
 	}		
